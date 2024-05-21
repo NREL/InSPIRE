4d3a2c98e78007359d3708d2b9c6a95c42071ab8
Silvana Ovaitt <silvana.ayala@nrel.gov>
2024-05-21 18:24:25 -0300

    debug martinmethod
    asdf

--- Studies/USMap_Doubleday_2024/Debug.py
+++ Studies/USMap_Doubleday_2024/Debug.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[ ]:
+
+
+import sys, platform
+import pvlib
+import PySAM
+import bifacial_radiance as br
+
+
+# In[ ]:
+
+
+print("Working on a ", platform.system(), platform.release())
+print("Python version ", sys.version)
+print("Pandas version ", pd.__version__)
+print("Pvlib version: ", pvlib.__version__)
+print("PySAM version: ", PySAM.__version__)
+print("bifacial_radiance version: ", br.__version__)
+
+
 # In[1]:
 
 
@@ -17,20 +37,7 @@ if NREL_API_KEY is None:
 import pvlib
 
 metdata, metadata = pvlib.iotools.get_psm3(
-    latitude=18.4671, longitude=-66.1185,
-    api_key=NREL_API_KEY,
-    email='silvana.ovaitt@nrel.gov',  # <-- any email works here fine
-    names='tmy', map_variables=False)
-metadata
-
-
-# In[3]:
-
-
-import pvlib
-
-metdata, metadata = pvlib.iotools.get_psm3(
-    latitude=18.4671, longitude=-66.1185,
+    latitude=44.25, longitude=-117.33999633789062,
     api_key=NREL_API_KEY,
     email='silvana.ovaitt@nrel.gov',  # <-- any email works here fine
     names='tmy', map_variables=True)
@@ -39,7 +46,7 @@ metadata
 
 # ## 2. Modeling with bifacial_radiance
 
-# In[4]:
+# In[3]:
 
 
 import pandas as pd
@@ -47,13 +54,13 @@ import matplotlib.pyplot as plt
 import bifacial_radiance as br
 
 
-# In[5]:
+# In[4]:
 
 
 br.__version__
 
 
-# In[6]:
+# In[5]:
 
 
 import os
@@ -67,13 +74,13 @@ if not os.path.exists(testfolder):
 print ("Your simulation will be stored in %s" % testfolder)
 
 
-# In[7]:
+# In[6]:
 
 
 radObj = br.RadianceObj('Sim3',path=testfolder)
 
 
-# In[8]:
+# In[7]:
 
 
 # Some of the names changed internally. While bifacial_radiance updates their expected names, we are renaming the values here
@@ -87,20 +94,20 @@ metdata['Albedo'] = metdata['albedo']
 
 # Use NSRDBWeatherData to enter data the downloaded data in dataframe and dictionary forma for meteorological data and metadata respectively
 
-# In[9]:
+# In[8]:
 
 
 #starttime can be 'MM_DD', or 'MM_DD_HH'
 metData = radObj.NSRDBWeatherData(metadata, metdata, starttime='11_08_09', endtime='11_08_11',coerce_year=2021)
 
 
-# In[10]:
+# In[9]:
 
 
 metData.datetime  # printing the contents of metData to see how many times got loaded.
 
 
-# In[11]:
+# In[10]:
 
 
 # -- establish tracking angles
@@ -122,31 +129,31 @@ trackerParams = {'limit_angle':50,
              }
 
 
-# In[12]:
+# In[11]:
 
 
 trackerdict = radObj.set1axis(**trackerParams)
 
 
-# In[13]:
+# In[12]:
 
 
 radObj.setGround(0.2) 
 
 
-# In[14]:
+# In[13]:
 
 
 radObj.gendaylit1axis()
 
 
-# In[15]:
+# In[14]:
 
 
 module=radObj.makeModule(name=modulename, x=1,y=2)
 
 
-# In[16]:
+# In[15]:
 
 
 sceneDict = {'pitch':pitch, 
@@ -158,19 +165,19 @@ sceneDict = {'pitch':pitch,
              }
 
 
-# In[17]:
+# In[16]:
 
 
 trackerdict = radObj.makeScene1axis(module=modulename,sceneDict=sceneDict)
 
 
-# In[18]:
+# In[17]:
 
 
 trackerdict = radObj.makeOct1axis()
 
 
-# In[19]:
+# In[18]:
 
 
 trackerdict = radObj.analysis1axis(customname = 'Module', 
@@ -178,19 +185,19 @@ trackerdict = radObj.analysis1axis(customname = 'Module',
                                    rowWanted=1)
 
 
-# In[20]:
+# In[19]:
 
 
 trackerdict = radObj.calculateResults(bifacialityfactor=0.7, agriPV=False)
 
 
-# In[21]:
+# In[20]:
 
 
 radObj.CompiledResults
 
 
-# In[22]:
+# In[21]:
 
 
 resolutionGround = 1  #meter. use 1 for faster test runs
@@ -207,13 +214,13 @@ trackerdict = radObj.analysis1axis(customname = 'Ground',
                                    modscanfront=modscanback, sensorsy=1)
 
 
-# In[24]:
+# In[ ]:
 
 
 trackerdict = radObj.calculateResults(bifacialityfactor=0.7, agriPV=True)
 
 
-# In[25]:
+# In[ ]:
 
 
 radObj.CompiledResults
@@ -224,7 +231,7 @@ radObj.CompiledResults
 # ## THIS WORKED WITH dev branch up to 4/22, and in the HPC versions we have.
 # 
 
-# In[26]:
+# In[ ]:
 
 
 ResultPVWm2Back = list(radObj.CompiledResults['Grear_mean'])
@@ -237,14 +244,14 @@ ResultWindSpeed = list(radObj.CompiledResults['Wind Speed'])
 ResultPVWm2Back
 
 
-# In[28]:
+# In[ ]:
 
 
 # In another wranch?? Thsi hsould have worked
 #list(radObj.CompiledResults['Module_temp'])
 
 
-# In[30]:
+# In[ ]:
 
 
 keys=list(trackerdict.keys())
@@ -259,19 +266,19 @@ for key in keys:
     
 
 
-# In[31]:
+# In[ ]:
 
 
 results = pd.DataFrame(list(zip(ResultPVWm2Back, ResultPVWm2Front)), columns = ["Back","Front"])
 
 
-# In[32]:
+# In[ ]:
 
 
 results['pitch']=trackerdict[key]['scene'].sceneDict['pitch']
 
 
-# In[33]:
+# In[ ]:
 
 
 results.to_pickle(results_path)
