00dd9eb04024dba7d71ce08001fca51e131f584e
tobin-ford <tford@nrel.gov>
2025-09-19 19:21:13 -0600

    Parameterize submit_state_conf and add env vars

--- Studies/USMap_Doubleday_2024/inspire-agrivolt-package/scripts/submit_state_conf.sh
+++ Studies/USMap_Doubleday_2024/inspire-agrivolt-package/scripts/submit_state_conf.sh
@@ -6,14 +6,33 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=17 # we changed this was previously 1, and changed from slurmcluster to localcluster
-#SBATCH --mem=80G
+#SBATCH --mem=100G
 #SBATCH --account=inspire
 #SBATCH --mail-user=tobin.ford@nrel.gov
 #SBATCH --mail-type=ALL
 
+# DASK SCHEDULER OPTIONS
+export DASK_DISTRIBUTED__WORKER__MEMORY__TARGET=0.60
+export DASK_DISTRIBUTED__WORKER__MEMORY__SPILL=0.70
+export DASK_DISTRIBUTED__WORKER__MEMORY__PAUSE=0.80
+export DASK_DISTRIBUTED__WORKER__MEMORY__TERMINATE=0.95
+
+# INTRA WORKER THREAD OPTIONS
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export BLIS_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
+unset PANDAS_USE_PYARROW
+unset PANDAS_STRING_STORAGE
+
 # jobname is defined in calling script (run_all_configs_state.sh)
 STATE=$1
 CONF=$2
+
+OUTPUT_DIR=/projects/inspire/PySAM-MAPS/v1/"$STATE"
+CONF_DIR=/home/tford/dev/InSPIRE/Studies/USMap_Doubleday_2024/SAM
 
 LOGFILE=logs/agrivolt-irr-$SLURM_JOB_NAME-$SLURM_JOB_ID.log
 ERRFILE=logs/agrivolt-irr-$SLURM_JOB_NAME-$SLURM_JOB_ID.err
@@ -21,7 +40,7 @@ ERRFILE=logs/agrivolt-irr-$SLURM_JOB_NAME-$SLURM_JOB_ID.err
 module load anaconda3
 source activate /home/tford/.conda-envs/geospatial
 
-#### do work ####
-agrivolt_ground_irradiance "$STATE" /projects/inspire/PySAM-MAPS/Full-Outputs/"$STATE"-rerun-fixed-sat /home/tford/dev/InSPIRE/Studies/USMap_Doubleday_2024/SAM --confs "$CONF" --port 22118 --workers 16 --partition shared --walltime 10:00:00
+#### RUN SIMULATION USING CLI ####
+agrivolt_ground_irradiance "$STATE" "$OUTPUT_DIR" "$CONF_DIR" --confs "$CONF" --port 22118 --workers 16 
 
 conda deactivate
